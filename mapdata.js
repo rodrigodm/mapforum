82a7c69d61a2ba314c224bb2566d91ba71521925
Rodrigo Dorta <rodrigodorta@gmail.com>
2021-08-24 18:09:52 -0300

    inicial commit

---
+++ mapdata.js
@@ -0,0 +1,175 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    BRA1294: {
+      name: "Goiás"
+    },
+    BRA1311: {
+      name: "São Paulo",
+      hover_color: "#dc1616"
+    },
+    BRA1313: {
+      name: "Pernambuco"
+    },
+    BRA576: {
+      name: "Acre"
+    },
+    BRA592: {
+      name: "Amazonas"
+    },
+    BRA593: {
+      name: "Maranhão"
+    },
+    BRA594: {
+      name: "Pará"
+    },
+    BRA595: {
+      name: "Rondônia"
+    },
+    BRA596: {
+      name: "Tocantins"
+    },
+    BRA599: {
+      name: "Distrito Federal"
+    },
+    BRA600: {
+      name: "Mato Grosso do Sul"
+    },
+    BRA601: {
+      name: "Minas Gerais"
+    },
+    BRA602: {
+      name: "Mato Grosso"
+    },
+    BRA612: {
+      name: "Rio Grande do Sul"
+    },
+    BRA613: {
+      name: "Paraná"
+    },
+    BRA614: {
+      name: "Santa Catarina"
+    },
+    BRA621: {
+      name: "Ceará"
+    },
+    BRA622: {
+      name: "Piauí"
+    },
+    BRA623: {
+      name: "Alagoas"
+    },
+    BRA624: {
+      name: "Bahia"
+    },
+    BRA625: {
+      name: "Espírito Santo"
+    },
+    BRA626: {
+      name: "Paraíba"
+    },
+    BRA627: {
+      name: "Rio de Janeiro"
+    },
+    BRA628: {
+      name: "Rio Grande do Norte"
+    },
+    BRA629: {
+      name: "Sergipe"
+    },
+    BRA670: {
+      name: "Roraima"
+    },
+    BRA681: {
+      name: "Amapá"
+    }
+  },
+  locations: {
+    "0": {
+      lat: -23.563,
+      lng: -46.655,
+      name: "São Paulo",
+      description: "cAt - Grupo de pesquisa UNESP",
+      url: "https://grupocat.wordpress.com/"
+    }
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {},
+  data: {
+    data: {}
+  }
+};
